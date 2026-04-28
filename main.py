ad8a95f59922b149f78c496bd4db7a4f5cd2e738
SafwanKS <safwansalim340@gmail.com>
2026-04-28 21:54:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from ddgs import DDGS
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/images")
+def image_search(q: str):
+    results = DDGS().images(q, max_results=6)
+    return {"results": [r["image"] for r in results]}
